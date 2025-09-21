e06cc783029c6f86346d0b653f4bd0c115b7fd9a
Tools-app <localhost.hutao@gmail.com>
2025-09-21 18:39:15 +0800

    feat: add logger system
    Signed-off-by: Tools-app <localhost.hutao@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::io::Write;
+
+use env_logger::Builder;
+
 fn main() {
-    println!("Hello, world!");
+    let mut builder = Builder::new();
+
+    builder.format(|buf, record| {
+        let local_time = chrono::Local::now();
+        let time_str = local_time.format("%Y-%m-%d %H:%M:%S%.3f").to_string();
+
+        writeln!(
+            buf,
+            "[{}] [{}] [{}] {}",
+            time_str,
+            record.level(),
+            record.target(),
+            record.args()
+        )
+    });
+    builder.filter_level(log::LevelFilter::Info).init();
 }
