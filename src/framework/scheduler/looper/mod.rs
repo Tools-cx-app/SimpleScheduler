6a43ee21cd83157e644e9ead4d92345c6f7f0307
Tools-app <localhost.hutao@gmail.com>
2025-09-27 02:18:47 +0800

    feat: add reflash topapp
    Signed-off-by: Tools-app <localhost.hutao@gmail.com>

--- src/framework/scheduler/looper/mod.rs
+++ src/framework/scheduler/looper/mod.rs
@@ -1,17 +1,33 @@
 use anyhow::Result;
 
-use crate::framework::config::data::ConfigData;
+use crate::framework::{config::data::ConfigData, scheduler::topapps::TopWatcher};
+
+struct SimpleSchedulerData {
+    topapps: TopWatcher,
+}
 
 pub struct Looper {
     config: ConfigData,
+    data: SimpleSchedulerData,
 }
 
 impl Looper {
     pub fn new(c: ConfigData) -> Self {
-        Self { config: c }
+        Self {
+            config: c,
+            data: SimpleSchedulerData {
+                topapps: TopWatcher::new(),
+            },
+        }
     }
 
-    pub fn enter_looper(&self) -> Result<()> {
-        loop {}
+    pub fn enter_looper(&mut self) -> Result<()> {
+        loop {
+            self.reflash_topapps();
+        }
+    }
+
+    fn reflash_topapps(&mut self) {
+        self.data.topapps.info();
     }
 }
