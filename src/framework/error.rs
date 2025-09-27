72c20434efc351f4f63992297855aac408066539
Tools-app <localhost.hutao@gmail.com>
2025-09-27 12:42:42 +0800

    feat: add config parse error handler
    Signed-off-by: Tools-app <localhost.hutao@gmail.com>

--- src/framework/error.rs
+++ src/framework/error.rs
@@ -4,4 +4,6 @@ use thiserror::Error as ThisError;
 pub enum Error {
     #[error("Missing {0} when binding looper")]
     SchedulerMissing(&'static str),
+    #[error("Failed to config parse {0}")]
+    ConfigParse(&'static str),
 }
--- src/framework/scheduler/looper/mod.rs
+++ src/framework/scheduler/looper/mod.rs
@@ -4,7 +4,7 @@ use anyhow::Result;
 use log::{error, info};
 
 use crate::{
-    framework::{config::Config, scheduler::topapps::TopWatcher},
+    framework::{Error, config::Config, scheduler::topapps::TopWatcher},
     msic::get_process_name_by_pid,
 };
 
@@ -41,7 +41,7 @@ impl Looper {
 
             let pid = self.data.topapps.pids()[0];
             let name = get_process_name_by_pid(pid)?;
-            let mode = self.list_include_target(&name);
+            let mode = self.list_include_target(&name)?;
 
             info!("New buffer for {name}, mode {mode}");
             self.reflash_topapps();
@@ -52,22 +52,21 @@ impl Looper {
         self.data.topapps.info();
     }
 
-    fn list_include_target(&mut self, target: &str) -> SimpleSchedulerMode {
+    fn list_include_target(&mut self, target: &str) -> Result<SimpleSchedulerMode, Error> {
         let config = self.config.config().config.clone();
         if config.bablance.contains(target) {
-            SimpleSchedulerMode::Bablance
+            Ok(SimpleSchedulerMode::Bablance)
         } else if config.powersave.contains(target) {
-            SimpleSchedulerMode::Powersave
+            Ok(SimpleSchedulerMode::Powersave)
         } else if config.performance.contains(target) {
-            SimpleSchedulerMode::Performance
+            Ok(SimpleSchedulerMode::Performance)
         } else {
             match config.general.as_str() {
-                "powersave" => SimpleSchedulerMode::Powersave,
-                "bablance" => SimpleSchedulerMode::Bablance,
-                "performance" => SimpleSchedulerMode::Performance,
+                "powersave" => Ok(SimpleSchedulerMode::Powersave),
+                "bablance" => Ok(SimpleSchedulerMode::Bablance),
+                "performance" => Ok(SimpleSchedulerMode::Performance),
                 _ => {
-                    error!("general config error");
-                    panic!();
+                    return Err(Error::ConfigParse("general option"));
                 }
             }
         }
