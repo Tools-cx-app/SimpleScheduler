95f9e3db7ba8a0237e01459fd2b2c22a699713f5
Tools-app <localhost.hutao@gmail.com>
2025-10-04 17:41:21 +0800

    feat: add freqs option in ConfigData
    Signed-off-by: Tools-app <localhost.hutao@gmail.com>

--- src/framework/config/data.rs
+++ src/framework/config/data.rs
@@ -5,6 +5,7 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct ConfigData {
     pub config: Config,
+    pub freqs: Freqs,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -13,4 +14,12 @@ pub struct Config {
     pub powersave: HashSet<String>,
     pub balance: HashSet<String>,
     pub performance: HashSet<String>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Freqs {
+    pub general: (isize, isize),
+    pub powersave: (isize, isize),
+    pub balance: (isize, isize),
+    pub performance: (isize, isize),
 }
