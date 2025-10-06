fdb4484b96da423808a4a4f7f02a51eb9baae5a1
Tools-app <localhost.hutao@gmail.com>
2025-10-06 20:54:35 +0800

    fix: fix typo
    Signed-off-by: Tools-app <localhost.hutao@gmail.com>

--- src/framework/scheduler/looper/fmt.rs
+++ src/framework/scheduler/looper/fmt.rs
@@ -6,7 +6,7 @@ impl fmt::Display for SimpleSchedulerMode {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Self::Powersave => write!(f, "powersave"),
-            Self::Bablance => write!(f, "bablance"),
+            Self::Balance => write!(f, "balance"),
             Self::Performance => write!(f, "performance"),
         }
     }
