b034a72ff98cbc091e4d1971c35323b68cdcb5e0
Tools-app <localhost.hutao@gmail.com>
2025-10-06 21:30:37 +0800

    feat: trying fix failed to get wake status
    Signed-off-by: Tools-app <localhost.hutao@gmail.com>

--- src/framework/scheduler/wake.rs
+++ src/framework/scheduler/wake.rs
@@ -1,26 +1,18 @@
-use std::time::{Duration, Instant};
+use std::{
+    process::Command,
+    time::{Duration, Instant},
+};
 
-use dumpsys_rs::Dumpsys;
-
-const RETRY_TIME: Duration = Duration::from_secs(1);
 const REFRESH_TIME: Duration = Duration::from_millis(500);
 
 pub struct Wake {
-    dumper: Dumpsys,
     last_update: Instant,
     status: bool,
 }
 
 impl Wake {
     pub fn new() -> Self {
-        let dumper = loop {
-            match Dumpsys::new("power") {
-                Some(s) => break s,
-                None => std::thread::sleep(RETRY_TIME),
-            }
-        };
         Self {
-            dumper,
             last_update: Instant::now(),
             status: bool::default(),
         }
@@ -28,14 +20,19 @@ impl Wake {
 
     pub fn info(&mut self) -> bool {
         if self.last_update.elapsed() > REFRESH_TIME {
-            let dump = loop {
-                match self.dumper.dump(&[""]) {
-                    Ok(dump) => break dump,
+            let dump = {
+                let command = match Command::new("dumpsys").arg("power").output() {
+                    Ok(o) => o,
                     Err(e) => {
-                        log::error!("Failed to dump power: {e}, retrying");
-                        std::thread::sleep(RETRY_TIME);
+                        log::debug!("Failed to dump power status: {e}");
+                        return true;
                     }
+                };
+                if !command.status.success() {
+                    log::debug!("Failed to dump power status");
+                    return true;
                 }
+                String::from_utf8_lossy(&command.stdout).to_string()
             };
 
             self.status = Self::parse_info(&dump);
