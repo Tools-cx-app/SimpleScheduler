9a2b23b6564518380b7bd217b5dee197ea7fa0f7
Tools-app <localhost.hutao@gmail.com>
2025-10-06 20:46:13 +0800

    fix(module): fix no log, and remove killall
    Signed-off-by: Tools-app <localhost.hutao@gmail.com>

--- module/service.sh
+++ module/service.sh
@@ -1,9 +1,9 @@
 #!/system/bin/sh
 MODDIR=${0%/*}
+LOG=$MODDIR/run.log
 
 until [ -d $MODDIR ]; do
   sleep 1
 done
 
-killall fas-rs-next
 RUST_BACKTRACE=1 nohup $MODDIR/SimpleScheduler $MODDIR/config.toml >$LOG 2>&1 &
